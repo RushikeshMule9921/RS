fb7c1f4b7ae2e7b06dfc1d7ea76a058514feb06b
nick93201 <nm.nikhil@iiitb.ac.in>
2024-05-15 20:22:16 +0530

    Login page updated
    Added CSS to the elements in the login page need to connect with firebase for google login

--- frontend/RS1.1/jobquest/src/login.js
+++ frontend/RS1.1/jobquest/src/login.js
@@ -1,15 +1,23 @@
 import React from 'react';
-
+import Navbar from './navbar';
+import Footer from './footer';
+import './login.css'
 class login extends React.Component {
   render() {
     return (
+     <div>
+     <Navbar/>
       <div className="login-container">
-        <div className="login-form">
-          <input type="text" placeholder="Enter your username or email" />
-        </div>
+      <div className="login-message">
+        <h1>Just one click away!</h1>
+        <p>By logging in with your Gmail account, you're taking the first step towards personalized job recommendations tailored to your skills and experience. Let's find your dream job together!</p>
+
         <div className="login-button">
           <button>Login via Google Account</button>
         </div>
+      </div>
+      </div>
+      <Footer/>
       </div>
     );
   }
