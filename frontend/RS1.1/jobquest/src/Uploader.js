dbc7306b0a6cf7bdcd1aa7d90b30b25d41ee26ba
nick93201 <nm.nikhil@iiitb.ac.in>
2024-05-16 18:59:44 +0530

    added recommedations page

--- frontend/RS1.1/jobquest/src/Uploader.js
+++ frontend/RS1.1/jobquest/src/Uploader.js
@@ -2,15 +2,35 @@ import React, { useState } from 'react';
 import Navbar from './navbar';
 import Footer from './footer';
 import './Uploader.css';
+import { useNavigate } from 'react-router-dom';
 import { auth } from './firebase'; // Import your Firebase authentication module
+import axios from 'axios';
 
 const Uploader = () => {
   const [resumeFile, setResumeFile] = useState(null);
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
 
   const onFileChange = (event) => {
     setResumeFile(event.target.files[0]);
   };
+  const sendResumeToBackend = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('resume', resumeFile);
 
+      const response = await axios.post('http://127.0.0.1:5000/api/process-resume', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+
+      setMessage(response.data.message);
+      navigate(`/recommendations?data=${encodeURIComponent(JSON.stringify(response.data.jobRecommendations))}`);
+    } catch (error) {
+      console.error('Error sending resume to backend:', error);
+    }
+  };
   const handleSignOut = async () => {
     try {
       await auth.signOut(); // Sign out the user
@@ -32,7 +52,7 @@ const Uploader = () => {
           </p>
           <div className="Upload">
             <input type="file" accept=".pdf,.doc,.docx" onChange={onFileChange} />
-            <button>Upload</button>
+            <button onClick={sendResumeToBackend}>Upload</button>
             <button onClick={handleSignOut}>Sign Out</button> {/* Add the sign-out button */}
           </div>
         </div>
